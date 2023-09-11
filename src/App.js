6e241af9c1ee266fc655e890586ea843958e32d4
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-09-12 00:18:13 +0530

    Added Base Files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Box1 from './components/box1/Box1';
+import Box2 from './components/box2/Box2';
+
+
+function App() {
+  return (
+    <>
+    
+    <header>
+        <h1>JSS Speech and Hearing</h1>
+        <h1>Dept Of Audilogy</h1>
+        <h2>Hearing Wellness</h2>
+    </header>
+    
+    <Box1 />
+    <Box2 />
+
+    </>
+    
+  );
+}
+
+export default App;
